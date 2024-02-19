271ee9f613dbcacc81c14371645950ed27f05962
Ben Saacks <108644584+Blancher@users.noreply.github.com>
2024-02-19 09:41:43 -0800

    Update UserRankings.js

--- frontend/src/pages/UserRankings.js
+++ frontend/src/pages/UserRankings.js
@@ -1,18 +1,20 @@
-import {useEffect, useState} from 'react';
+import {useContext, useEffect, useState} from 'react';
 import RankingCard from '../components/RankingCard';
 import useRouteProtection from '../hooks/useRouteProtection';
+import {context} from '../store/context';
 import useHttp from '../hooks/useHttp';
 import Loading from '../components/Loading';
 
 export default function UserRankings() {
     useRouteProtection();
+    const ctx = useContext(context);
     const [error, isLoading, sendRequest] = useHttp();
     const [rankings, setRankings] = useState([]);
 
     useEffect(() => {
         const dataFetcher = async() => {
-            const response = await sendRequest('ranking');
-            setRankings(response.rankings);
+            const response = await sendRequest('ranking/empty');
+            setRankings(response.rankings.filter(ranking => ranking.creator.id === ctx.userId));
         };
 
         dataFetcher();
