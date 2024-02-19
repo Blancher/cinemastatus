dd6ef3841a5361f5f66af313040ce1a64daba168
Ben Saacks <108644584+Blancher@users.noreply.github.com>
2024-02-19 12:53:05 -0800

    Update rankingRoutes.js

--- backend/routes/rankingRoutes.js
+++ backend/routes/rankingRoutes.js
@@ -6,8 +6,8 @@ const checkAuth = require('../middleware/checkAuth');
 const router = express.Router();
 
 router.get('/:title', rankingControllers.getRankings);
+router.get('/single/:rankingId', rankingControllers.getRankingById);
 router.get('/:seriesId/:title', rankingControllers.getRankingsById);
-router.get('/:rankingId', rankingControllers.getRankingById);
 router.use(checkAuth);
 router.post('/:seriesId', [check('title').isLength({min: 1, max: 30})], rankingControllers.createRanking);
 router.patch('/:rankingId', [check('title').isLength({min: 1, max: 30})], rankingControllers.updateRanking);
